805e7c488611707413d0cb026a250d8df82a9bf4
takumitakeno <matchball.shine@gmail.com>
2020-05-17 02:40:19 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   resources :microposts
   resources :users
-  root 'application#goodbye'
+  
 end
